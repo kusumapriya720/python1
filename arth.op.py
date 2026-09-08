0f26ff91aae65cc7be37fc0792e683bbf3a759db
kusumapriya720 <kovikusumapriya@gmail.com>
2026-09-08 10:34:16 +0530

    Add files via upload

---
+++ arth.op.py
@@ -0,0 +1,17 @@
+#electric city bill calculator
+units = int(input("Enter electricity unnits"))
+
+rate = 6
+
+bill = units * rate
+
+print("Electricity Bill:", bill)
+
+#travel expense calculator
+travel = float (input("travel expense: "))
+food = float(input("Food expense: "))
+hotel =float(input("Hotel expense: "))
+
+total = travel + food + hotel 
+
+print("Total Expence:", total)
